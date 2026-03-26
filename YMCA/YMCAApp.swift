732b494b5dcba52c9db8ca1fb9f8cb48feff92ab
Giovanni <giofioroni@gmail.com>
2026-03-26 16:58:10 +0000

    Initial Commit

---
+++ YMCA/YMCAApp.swift
@@ -0,0 +1,17 @@
+//
+//  YMCAApp.swift
+//  YMCA
+//
+//  Created by Giovanni Fioroni on 26/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct YMCAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
